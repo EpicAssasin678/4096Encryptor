4cf72c4794e6e3191c9035250e15559269280bb1
Zachery Uporsky <epicdjzach@gmail.com>
2021-10-29 09:25:54 -0400

    Initial commit.

---
+++ src/App.java
@@ -0,0 +1,15 @@
+public class App {
+
+    /**
+     * Author: darkf0x
+     * Date: 10/29/21
+     * 
+     * 4096 Encryptor is an encryption algorthim.
+     * TODO: create main object                            
+     */
+
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
